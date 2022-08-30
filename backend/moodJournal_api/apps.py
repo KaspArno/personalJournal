e853a37dbaf6d1d13fd8c75991d79ea737c1850c
KaspArno <kasper.skjeggestad@gmail.com>
2022-08-30 22:06:36 +0200

    adding first models

---
+++ backend/moodJournal_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MoodjournalApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'moodJournal_api'
---
+++ backend/moodJournal_api/models.py
@@ -0,0 +1,21 @@
+from pyexpat import model
+from django.db import models
+
+
+class Mood(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(blank=False, max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Log(models.Model):
+    date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    note = models.TextField(blank=True)
+    mood = models.ForeignKey('Mood', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return (self.date)
